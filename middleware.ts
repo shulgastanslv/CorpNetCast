bde0f5848f3c4126b1ddeac8c2534ca3ff128fc7
shulgastanslv <shulgastanslv@gmail.com>
2024-03-01 21:37:00 +0300

    feat: update middleware

--- middleware.ts
+++ middleware.ts
@@ -27,11 +27,11 @@ export default auth((req : any) => {
         return null;
     }
 
-    if (!isLoggedIn && !isPublicRoute) {
-        return Response.redirect(new URL("/auth/login", nextUrl));
-    }
+    // if (!isLoggedIn && !isPublicRoute) {
+    //     return Response.redirect(new URL("/auth/login", nextUrl));
+    // }
 
-    return null;
+    return Response.redirect(new URL("/auth/login", nextUrl));
 })
 
 // Optionally, don't invoke Middleware on some paths
